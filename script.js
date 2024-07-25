8df7105387539bfd128cca290c34daa2be64fcc1
GustavoBastos04 <gubasouza2004@gmail.com>
2024-07-25 16:24:29 -0300

    Inital Panel to Change number of Squares.
    The Prototype of the Panel that allows user to change the number of squares up to 100x100 of Sketchpad. By now the style is not finished neither has an action yet.

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 // Body
 let body = document.querySelector("body");
 body.style.display = "flex";
+body.style.flexDirection = "column";
+body.style.gap = "15px";
 body.style.alignItems = "center";
 body.style.justifyContent = "center";
+
+// User Grid Size Input
+let inputPanel = document.createElement("div");
+
+inputPanel.style.display = "flex";
+inputPanel.style.alignItems = "center";
+inputPanel.style.gap = "10px";
+inputPanel.style.fontSize = "20px";
+inputPanel.style.fontWeight = "bold";
+
+let span = document.createElement("span");
+span.textContent = "Type grid size:";
+
+let input = document.createElement("input");
+input.type = "number";
+input.min = "1";
+input.max = "100";
+input.placeholder = "1 to 100";
+input.style.fontSize = "20px";
+
+let button = document.createElement("button");
+button.textContent = "Change";
+button.style.borderRadius = "15px";
+button.style.fontWeight = "bold";
+button.style.fontSize = "20px";
+
+inputPanel.appendChild(span);
+inputPanel.appendChild(input);
+inputPanel.appendChild(button);
+
+body.appendChild(inputPanel);
 
 // Grid
 let grid = document.createElement("div");
@@ -11,6 +44,9 @@ grid.classList.add("main-container");
 grid.style.display = "flex";
 grid.style.flexDirection = "column";
 
+grid.style.width = "600px";
+grid.style.height = "600px";
+
 body.appendChild(grid);
 
 for (let i = 1; i <= 16; i++) {
@@ -18,6 +54,10 @@ for (let i = 1; i <= 16; i++) {
     // Grid Rows
     let gridRow = document.createElement("div");
     gridRow.style.display = "flex";
+    
+    gridRow.style.width ="100%";
+    gridRow.style.height = "100%";
+
     gridRow.classList.add("grid-row");
     gridRow.classList.add(`${i}`);
 
@@ -33,8 +73,9 @@ for (let i = 1; i <= 16; i++) {
         gridElement.style.borderStyle = "solid"
         gridElement.style.borderColor = "black";
         gridElement.style.borderWidth = "thin";
-        gridElement.style.width = "35px";
-        gridElement.style.height = "35px";
+        
+        gridElement.style.width = "100%";
+        gridElement.style.height = "100%";
 
         gridRow.appendChild(gridElement);
     }
